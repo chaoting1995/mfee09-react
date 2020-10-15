d240c3c45b6c2c89866dbc420f90fe3c465b1f38
chaoting <02115214@gm.scu.edu.tw>
2020-10-15 15:05:20 +0800

    1015

--- code-example/1015/7.練習題：日期選擇後計算年紀-final/App.js
+++ code-example/1015/7.練習題：日期選擇後計算年紀-final/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import MyNavbar from './components/MyNavbar'
 import MyFooter from './components/MyFooter'
 import AgeCheck from './components/AgeCheck'
-import TodoApp from './components/TodoApp'
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
       <MyNavbar />
       <main className="flex-shrink-0">
         <div className="container">
-          <TodoApp />
+          <AgeCheck />
         </div>
       </main>
       <MyFooter />
